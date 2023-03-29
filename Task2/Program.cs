eb6058e2fc415c79ee6b255965f18c26e844b030
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-03-29 20:45:27 +0300

    Second task commit

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Введите число: ");
+int N=int.Parse(Console.ReadLine());
+int i=10;
+if(N>i*10)
+{
+    while(N/i<100)
+    {
+        System.Console.WriteLine(N%i);
+    }
+        i=i*10;   
+}
+else
+{
+    System.Console.WriteLine("третьей цифры нет");
+    }
