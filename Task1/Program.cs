f6234ce26094d21bad61ddad026398dbf4feeab3
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-03-29 20:44:53 +0300

    First task commit

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+System.Console.WriteLine("введите трехзначное число");
+int N=int.Parse(Console.ReadLine());
+int K=N/10;
+System.Console.WriteLine(K%10);
