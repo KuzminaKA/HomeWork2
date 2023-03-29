a263a06ad1287e0cf7e238b3e20a353ab42db8fb
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-03-29 20:46:21 +0300

    Third task commit

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//адача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+System.Console.WriteLine("введите цифру дня недели");
+int i=int.Parse(Console.ReadLine());
+
+bool Weekday (int a,int b)
+{
+    return a<=b;
+}
+
+if (Weekday(i,5))
+{
+    System.Console.WriteLine("нет");
+}
+else
+{
+    System.Console.WriteLine("ДА!");
+}
